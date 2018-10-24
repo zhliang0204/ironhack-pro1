e59aee74956468b5fd71dd212be9047c1a439cd3
Luna Zhang <zhliang0204@163.com>
2018-10-24 16:42:40 +0200

    sixth commit

--- js/main.js
+++ js/main.js
@@ -83,4 +83,19 @@ window.onload = function() {
             chooseDirection();
         }, 2000/timeSpan)
     }
+
+    // score list part
+    // show and clear score
+    ul = $("#score-list");
+    for(var i = 1; i < storage.length+1; i++){
+        var curPlayer = "player" + i;
+        var curScore = storage.getItem(curPlayer)
+        var curli = $("<li class='list-group-item'></li>");
+        curli.text(curPlayer + " : " + curScore);
+        ul.append(curli);
+    }
+
+    $("#clearScore").click(function(){
+        storage.clear();
+    })
 }
